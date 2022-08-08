df30633ddd31b8c5dd7621377c4b7f16784d7062
Asad <Asad@DESKTOP-SLKPD51>
2022-08-08 14:11:37 +0530

    D.T Version 3

--- MyFirstJavaProject/src/MainView.java
+++ MyFirstJavaProject/src/MainView.java
@@ -1,4 +1,21 @@
-
 public class MainView {
-
+	public static void main(String[] args) {
+		
+	    int myInt = 32767;
+	    System.out.println(myInt);
+	    
+	    char myChar = (char) -128;
+	    System.out.println(myChar);
+	    
+	    int myShort = 2147484;
+	    System.out.println(myShort);
+	    
+	    
+	    int myLongInt = 2147483647;
+	    System.out.println(myLongInt);
+	    
+	
+	  
+	}
+	
 }
